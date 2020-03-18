fc0b97604803f8af9fea85fce34173d60018a67a
Aditya Sinha <aditya.sinha@comviva.com>
2020-03-18 17:25:19 +0530

    fading text done

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -9,6 +9,12 @@ class Logo extends Component {
   render() {
     return (
       <div className="container">
+        <span className="js-nametag" style={{ left: "30%" }}>
+          Hey! This is your Virtual Assistant
+        </span>
+        <span className="js-nametag" style={{ left: "41%" }}>
+          I'm listening . . .
+        </span>
         <img className="logo1" src={brain} alt="..." />
         <img className="logo2" src={circle} alt="..." />
         <div className="clock">
